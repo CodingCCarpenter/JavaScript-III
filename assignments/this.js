fd6018da2a664462c6f3f687957ca60333e755bc
CodingCCarpenter <christineassists@aol.com>
2019-08-15 15:41:07 -0500

    polish up this keyword page after watching Josh Knell video

--- assignments/this.js
+++ assignments/this.js
@@ -11,41 +11,42 @@
 
 // Principle 1
 function introduction(name) {
-    console.log(this);
-    return name;
-  }
-  introduction("Daenerys Targaryen");
+    return `Hello ${this.name}`;
+  };
+  console.log(introduction("Daenerys Targaryen"));
 // code example for Window Binding
 
 // Principle 2
-const thisObj = {
+const hogwartsStudents = {
+    name: 'Luna Lovegood',
     greet: 'Why hello there!',
-    sayHello: function(name) {
-      console.log(`${this.greet} My name is ${name}, what's your name?`);
+    sayHello: function() {
+      console.log(`${this.greet} My name is ${this.name}, what's your name?`);
       console.log(this);
     }
   };
-  thisObj.sayHello('Hermione Granger');
+  console.log(hogwartsStudents.sayHello());
+
 // code example for Implicit Binding
 
 // Principle 3
-function nicePerson(speaker) {
-    this.greeting = 'My name is ';
-    this.speaker = speaker;
-    this.greet = function() {
-      console.log(`${this.greeting} ${this.speaker}`);
-      console.log(this);
-    };
-  }
-  
-  const neville = new nicePerson('Seamus');
-  const seamus = new nicePerson('Neville');
-  
-  console.log(neville.greet());
-  console.log(seamus.greet());
-// code example for New Binding
+const student = {
+  'name': 'Hermione Granger'
+}
+const talents = ['potions', 'arithmacy', 'transfiguration'];
+
+function brag(talent1, talent2, talent3) {
+  return `Hello! My name is ${this.name} and I excel in ${talent1}, ${talent2}, and ${talent3}`;
+}
+
+console.log(brag.call(student, ...talents));
+
+
+// code example for Explicit Binding
 
 // Principle 4
-neville.greet.call(seamus);
-seamus.greet.call(neville);
-// code example for Explicit Binding
+
+const addToPensieve = brag.bind(student, ...talents);
+console.log(addToPensieve());
+
+// code example for New Binding
